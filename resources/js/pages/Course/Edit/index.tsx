cbb0fce2aa6238d4a1ca88debbdc43d7568dacab
Gifino Thoriq <gifinothoriq@Gifinos-MacBook-Air.local>
2025-06-23 10:39:48 +0700

    fix: layout issue

--- resources/js/pages/Course/Edit/index.tsx
+++ resources/js/pages/Course/Edit/index.tsx
@@ -36,43 +36,46 @@ export default function Edit(){
     return(
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Edit Course"/>
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+                <form className="flex flex-col gap-6" onSubmit={submit}>
+                    <div className="grid gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="name">Course Name</Label>
+                            <Input
+                                id="name"
+                                type="text"
+                                autoFocus
+                                tabIndex={1}
+                                autoComplete="name"
+                                value={data.name}
+                                onChange={(e) => setData('name', e.target.value)}
+                            />
+                            <InputError message={errors.name} />
+                        </div>
 
-            <form className="flex flex-col gap-6" onSubmit={submit}>
-                <div className="grid gap-6">
-                    <div className="grid gap-2">
-                        <Label htmlFor="name">Course Name</Label>
-                        <Input
-                            id="name"
-                            type="text"
-                            autoFocus
-                            tabIndex={1}
-                            autoComplete="name"
-                            value={data.name}
-                            onChange={(e) => setData('name', e.target.value)}
-                        />
-                        <InputError message={errors.name} />
+                        <div className="grid gap-2">
+                            <Label htmlFor="description">Course Description</Label>
+                            <Input
+                                id="description"
+                                type="text"
+                                autoFocus
+                                tabIndex={1}
+                                autoComplete="description"
+                                value={data.description}
+                                onChange={(e) => setData('description', e.target.value)}
+                            />
+                            <InputError message={errors.description} />
+                        </div>
+
+                        <div>
+                            <Button type="submit" className="mt-4" tabIndex={4} disabled={processing}>
+                                {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+                                Edit Course
+                            </Button>
+                        </div>
                     </div>
-
-                    <div className="grid gap-2">
-                        <Label htmlFor="description">Course Description</Label>
-                        <Input
-                            id="description"
-                            type="text"
-                            autoFocus
-                            tabIndex={1}
-                            autoComplete="description"
-                            value={data.description}
-                            onChange={(e) => setData('description', e.target.value)}
-                        />
-                        <InputError message={errors.description} />
-                    </div>
-
-                    <Button type="submit" className="mt-4 w-full" tabIndex={4} disabled={processing}>
-                        {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                        Edit Course
-                    </Button>
-                </div>
-            </form>
+                </form>               
+            </div>
         </AppLayout>
     )
 }
